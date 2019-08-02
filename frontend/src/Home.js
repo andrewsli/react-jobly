758b3a6b1651efff07f01f89f6c4eba14cbe1f9d
Andrew Li <andrewli0725@gmail.com>
2019-08-02 16:42:20 -0700

    fixed home page currUser check

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import UserContext from './UserContext';
 
 class Home extends Component {
+  static contextType = UserContext;
+
   render() {
+    const currUser = this.context;
     return (
       <div>
         <h1>Jobly</h1>
         <p>All the jobs in one, convenient place.</p>
-        {this.props.currUser ? <h2>Welcome Back!</h2> : <Link to="/login">Log in</Link>}
+        {currUser ? <h2>Welcome Back!</h2> : <Link to="/login">Log in</Link>}
 
       </div>
     );
